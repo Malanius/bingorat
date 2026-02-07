03a22773bd96ec2f1a2bb99b8b09fa5abba5d2c4
Michal Slota <malaniusprivierre@gmail.com>
2026-02-07 18:27:20 +0100

    refactor: inline keycode matches for same actions

--- src/input.rs
+++ src/input.rs
@@ -13,16 +13,11 @@ pub enum Action {
 impl Action {
     pub fn from_key(key: KeyEvent) -> Self {
         match key.code {
-            KeyCode::Up => Action::MoveCursor(Direction::Up),
-            KeyCode::Char('k') => Action::MoveCursor(Direction::Up),
-            KeyCode::Down => Action::MoveCursor(Direction::Down),
-            KeyCode::Char('j') => Action::MoveCursor(Direction::Down),
-            KeyCode::Left => Action::MoveCursor(Direction::Left),
-            KeyCode::Char('h') => Action::MoveCursor(Direction::Left),
-            KeyCode::Right => Action::MoveCursor(Direction::Right),
-            KeyCode::Char('l') => Action::MoveCursor(Direction::Right),
-            KeyCode::Enter => Action::Toggle,
-            KeyCode::Char(' ') => Action::Toggle,
+            KeyCode::Up | KeyCode::Char('k') => Action::MoveCursor(Direction::Up),
+            KeyCode::Down | KeyCode::Char('j') => Action::MoveCursor(Direction::Down),
+            KeyCode::Left | KeyCode::Char('h') => Action::MoveCursor(Direction::Left),
+            KeyCode::Right | KeyCode::Char('l') => Action::MoveCursor(Direction::Right),
+            KeyCode::Enter | KeyCode::Char(' ') => Action::Toggle,
             KeyCode::Char('r') => Action::Reset,
             KeyCode::Char('q') => Action::Quit,
             _ => Action::None,

46fa4092d54b49968538a05bbb84e7bdfb100947
Michal Slota <malaniusprivierre@gmail.com>
2026-06-29 16:46:23 +0200

    fix(persistence): remove pointless string join and split hop

--- src/persistence.rs
+++ src/persistence.rs
@@ -6,24 +6,17 @@ pub fn parse_predictions(path: &str) -> Bingo {
     let file = PathBuf::from(path);
     let contents = fs::read_to_string(file).expect("Failed to read file");
 
-    let filtered_contents: String = contents
+    let filtered_contents = contents
         .lines()
         .filter(|line| !line.starts_with('#') && !line.trim().is_empty())
-        .collect::<Vec<&str>>()
-        .join("\n");
-
-    // println!("File contents:\n{filtered_contents}");
-
-    // let output_file = PathBuf::from("filtered_predictions.brat");
-    // fs::write(&output_file, filtered_contents).expect("Failed to write file");
-    // println!("Filtered contents written to: {}", output_file.display());
+        .collect::<Vec<&str>>();
 
     let mut title = String::new();
     let mut win_message = String::new();
     let mut size = 0;
     let mut cells: Vec<Cell> = Vec::new();
 
-    filtered_contents.split('\n').for_each(|line| match line {
+    filtered_contents.into_iter().for_each(|line| match line {
         l if line.starts_with("size:") => size = parse_size(l),
         l if line.starts_with("name:") => title = parse_string_param(l),
         l if line.starts_with("win_message:") => win_message = parse_string_param(l),
@@ -34,7 +27,13 @@ pub fn parse_predictions(path: &str) -> Bingo {
 
     // Free cell always has to be in the middle (ommented as such in the brat file template)
     let free_cell_index = (size * size) / 2;
-    Bingo::new(&title, &win_message, size, cells, (free_cell_index / size, free_cell_index % size)) 
+    Bingo::new(
+        &title,
+        &win_message,
+        size,
+        cells,
+        (free_cell_index / size, free_cell_index % size),
+    )
 }
 
 fn parse_size(size_line: &str) -> usize {
